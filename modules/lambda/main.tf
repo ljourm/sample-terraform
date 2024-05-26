bbd24df4f5ea3936772cd325af8ed0eadf0915e1
ljourm <ljourm@gmail.com>
2024-05-27 00:45:46 +0900

    feat: Lambdaに関数URLの設定を追加

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -88,3 +88,8 @@ resource "aws_lambda_function" "lambda_function" {
     }
   }
 }
+
+resource "aws_lambda_function_url" "lambda_function_url" {
+  function_name      = aws_lambda_function.lambda_function.function_name
+  authorization_type = "NONE"
+}
