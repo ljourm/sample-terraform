d1013cfe63d761bf5f5da52204e585173a509661
ljourm <ljourm@gmail.com>
2024-05-30 23:01:33 +0900

    feat: cloudfront_s3のmoduleを直下への配置に変更

--- cloudfront_s3/main.tf
+++ cloudfront_s3/main.tf
@@ -1,19 +1,19 @@
 module "s3_bucket" {
-  source = "../modules/s3_bucket"
+  source = "./modules/s3_bucket"
 
   project     = var.project
   environment = var.environment
 }
 
 module "cloudfront" {
-  source = "../modules/cloudfront_distribution"
+  source = "./modules/cloudfront_distribution"
 
   domain_name  = module.s3_bucket.bucket_regional_domain_name
   s3_bucket_id = module.s3_bucket.name
 }
 
 module "s3_bucket_policy" {
-  source = "../modules/s3_bucket_policy"
+  source = "./modules/s3_bucket_policy"
 
   s3_bucket_id                = module.s3_bucket.name
   s3_bucket_arn               = module.s3_bucket.arn
