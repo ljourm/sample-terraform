21d8cb9c4519ac923c89c1428a91ac1511300818
ljourm <ljourm@gmail.com>
2024-05-30 23:03:30 +0900

    feat: api_lambda_sesのmoduleを直下への配置に変更

--- api_lambda_ses/main.tf
+++ api_lambda_ses/main.tf
@@ -1,18 +1,18 @@
 module "sample_ses" {
-  source = "../modules/ses"
+  source = "./modules/ses"
 
   domain = var.domain
 }
 
 module "sample_ses_for_route53" {
-  source = "../modules/route53_for_ses"
+  source = "./modules/route53_for_ses"
 
   domain = var.domain
   dkim_tokens = module.sample_ses.dkim_tokens
 }
 
 module "samle_lambda" {
-  source = "../modules/lambda"
+  source = "./modules/lambda"
 
   project = var.project
   environment = var.environment
